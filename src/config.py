fac6674fd2a28cf98ffd4ce9b2a9ce0914ad6af9
Ananda Sarangaram <asarangaram@hotmail.com>
2023-06-23 18:26:49 +0530

    changes in app and config for /image ep

--- src/config.py
+++ src/config.py
@@ -9,3 +9,12 @@ class ConfigClass(object):
         SECRET_KEY = 'Secret!'
     PROPAGATE_EXCEPTIONS = True
     
+    # Flask-SQLAlchemy
+    password = os.environ['MYSQL_IMAGE_REPO_ADMIN_PW']
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://image_repo_admin:{password}@localhost/image_repo_db"
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    # File Save
+    FILE_STORAGE_LOCATION = "/home/anandas/storage/image_repo"
+    
