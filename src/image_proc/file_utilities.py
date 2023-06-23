ce5e9d609ac52deb0d163da37175bf3163f17794
Ananda Sarangaram <asarangaram@hotmail.com>
2023-06-23 18:02:15 +0530

    Utility Functions

---
+++ src/image_proc/file_utilities.py
@@ -0,0 +1,9 @@
+from io import BytesIO
+from werkzeug.datastructures import FileStorage
+
+def load_image_from_werkzeug_cache(im:FileStorage):
+    """ If successful, return the bytes, else raise exceptions"""
+    bytes_io = BytesIO()
+    im.save(bytes_io)
+    bytes_io.seek(0)
+    return bytes_io
