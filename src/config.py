a4dcaac41058be1106a8186e0575e71fc5946b96
Ananda S <4833102+asarangaram@users.noreply.github.com>
2023-08-07 11:09:19 +0530

    use environment variables

--- src/config.py
+++ src/config.py
@@ -1,20 +1,28 @@
 # config.py
 import os
 
+def get_required_env_variable(var_name):
+    value = os.environ.get(var_name)
+    if value is None:
+        raise ValueError(f"Environment variable '{var_name}' is not set.")
+    return value
+
 class ConfigClass(object):
-    APP_NAME = "Image Repo"
+    APP_NAME = get_required_env_variable('APP_NAME')
     try:
-        SECRET_KEY = os.environ['FLASK_SECRET_KEY1'] 
+        SECRET_KEY = get_required_env_variable('FLASK_SECRET_KEY1')
     except: 
         SECRET_KEY = 'Secret!'
     PROPAGATE_EXCEPTIONS = True
     
     # Flask-SQLAlchemy
-    password = os.environ['MYSQL_IMAGE_REPO_ADMIN_PW']
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://image_repo_admin:{password}@localhost/image_repo_db"
+    repo = get_required_env_variable('MYSQL_IMAGE_REPO')
+    user = get_required_env_variable('MYSQL_IMAGE_REPO_ADMIN')
+    password = get_required_env_variable('MYSQL_IMAGE_REPO_ADMIN_PW')
+
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{user}:{password}@localhost/{repo}"
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # File Save
-    FILE_STORAGE_LOCATION = "/home/anandas/storage/image_repo"
-    
+    FILE_STORAGE_LOCATION = get_required_env_variable('FILE_STORAGE_LOCATION')
